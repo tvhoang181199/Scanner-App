f6274a1b360bbf3f6ba3415545384a8ff7f930ca
vuhoang <vhtrinh73@gmail.com>
2020-11-20 17:37:22 +0700

    Initial Commit

---
+++ Scanner App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scanner App
+//
+//  Created by Trịnh Vũ Hoàng on 20/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
